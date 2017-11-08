1c7c0a2cb4e1166f9172f9f61e3f52780898a833
collo teng <netcollo@gmail.com>
2017-11-08 23:25:58 +0800

    doing 0002

--- 0002.py
+++ 0002.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-import random
+import random,pymysql
+
+
 
 n = 1
-
 while n <= 200:
     #print("%s:  %s" %(n, "".join(random.choices("ABCDEFGHJKMNPQRSTUVWXY1234567890",k=10))))
     print("".join(random.choices("ABCDEFGHJKMNPQRSTUVWXY1234567890",k=10)))
