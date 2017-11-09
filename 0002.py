773118a56dca28fd70437930b7f2a2e0a9555adc
teng <netcollo@gmail.com>
2017-11-09 10:19:17 +0800

    0002 done

--- 0002.py
+++ 0002.py
@@ -2,10 +2,15 @@
 
 import random,pymysql
 
+db = pymysql.connect("localhost", "root", "", "showmethecode")
 
+cursor = db.cursor()
 
 n = 1
 while n <= 200:
-    #print("%s:  %s" %(n, "".join(random.choices("ABCDEFGHJKMNPQRSTUVWXY1234567890",k=10))))
-    print("".join(random.choices("ABCDEFGHJKMNPQRSTUVWXY1234567890",k=10)))
+    sql = "Insert INTO 0002PromoCode (promocode, isused) values('" + "".join(random.choices("ABCDEFGHJKMNPQRSTUVWXY1234567890",k=10)) + "', '0')"
+    cursor.execute(sql)
     n += 1
+
+db.commit()
+db.close()
